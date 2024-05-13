7637de43f3d137141a404f1c83be054261247f09
Nitin Goswami <nitingoswami1900@gmail.com>
2024-05-14 00:16:40 +0530

    mod: react

--- nginx/frontend/src/App.js
+++ nginx/frontend/src/App.js
@@ -1,17 +1,9 @@
-// import VcpLogin from 'vcp-test/dist/components/VcoLogin';
-// import { VcpMonthlyPivot } from 'vcp-cal-components';
-import { Button } from 'vcp-github-components';
 import './App.css';
-// import {VcpLogin, Button} from "vcp-test"
 
 function App() {
   return (
     <div className="App">
       <h2>hello world</h2>
-      {/* <VcpLogin loginUrl={"https://contacts.virtusasystems.com/api/login/student"} subscriberCode='swtb'/> */}
-      {/* <Button>Hello</Button>
-      <VcpMonthlyPivot subscriberCode='ta'/> */}
-      <Button/>
     </div>
   );
 }
